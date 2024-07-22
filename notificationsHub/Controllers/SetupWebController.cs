7405c478b493cb9f0400a1cfb222d24858ae9a30
Leonardo da Silva Pipoli <leonardo.pipoli48@outlook.com>
2024-07-22 20:15:11 -0300

    Adicionar arquivos de projeto.

---
+++ notificationsHub/Controllers/SetupWebController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apresentacao.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class SetupWebController : ControllerBase
+  {
+  }
+}
